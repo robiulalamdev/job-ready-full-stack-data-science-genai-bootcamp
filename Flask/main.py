0b01b9015c00b8d0b88f41020eb33d38f91f2b5c
robiulalamdev <robiulalam762400@gmail.com>
2026-08-28 20:42:59 +0600

    Add initial Flask application with welcome route

---
+++ Flask/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def welcome():
+    return "Welcome to the Flask application!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9500)
